23332cf9ccaa7eb614f83976676e73ce6102148e
Sam <samsamuhfoot@gmail.com>
2025-02-10 18:27:13 +0100

    Fixing bugs in script.js

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ const imagezone = document.getElementById("image-zone")
 // Function used to generate a QRCode thanks to goqr.me/api/
 function generate(){
     var text = inputbox.value;
-    var image = document.createElement("img");
-    image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
-    imagezone.appendChild(image);
+    if (text === ""){
+        return;
+    }
+    else {
+        var image = document.createElement("img");
+        image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
+        imagezone.appendChild(image);
+    }
 }
 
 // The task is added if you left-click on the OK button
 generatebutton.addEventListener("click", generate);
 
 // The task is added if you press enter key
-input.addEventListener("keypress", function (event) {
+inputbox.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         generate();
     }
