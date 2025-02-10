2f187800224bd55588444aba6db01abad2e793c4
Sam <samsamuhfoot@gmail.com>
2025-02-10 18:23:07 +0100

    Editing all files and got a working QR code generator

---
+++ script.js
@@ -0,0 +1,22 @@
+// Element used by this script
+const inputbox = document.getElementById("input-box");
+const generatebutton = document.getElementById("generate-button");
+const imagezone = document.getElementById("image-zone")
+
+// Function used to generate a QRCode thanks to goqr.me/api/
+function generate(){
+    var text = inputbox.value;
+    var image = document.createElement("img");
+    image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
+    imagezone.appendChild(image);
+}
+
+// The task is added if you left-click on the OK button
+generatebutton.addEventListener("click", generate);
+
+// The task is added if you press enter key
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        generate();
+    }
+});
